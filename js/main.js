c11b1911ef3a323bbec99af11e6d698e80bc12b5
Lunopark <rostypod@gmail.com>
2021-08-09 19:16:27 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -9,3 +9,11 @@ $(function(){
        $('.mobile-nav').toggleClass('show-menu')
     })
 });
+
+$('#tabs__caption li').click(function(){
+   alert("Место вопроса");
+   if (!$(this).hasClass("active")) {
+     $('#tabs__caption li.active').removeClass("active");
+     $(this).addClass("active");
+   }
+});
